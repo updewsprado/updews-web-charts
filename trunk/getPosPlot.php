61a103d88cd8f8f8a6b8d536cf57e9864100ffff
updews.prado@gmail.com <updews.prado@gmail.com@e0b541cd-f2c1-ab39-7beb-506130f5bc63>
2014-08-08 06:03:07 +0000

    - "getPosPlot.php", returns data with "simulated" y-value - "positionPlot.html", displays column position graphs with appropriate scale to the x and y axes + "testPath.html" and "testPath2.html", for testing the line graphing in D3 + "testPlot.html", for testing the point plotting in D3

--- trunk/getPosPlot.php
+++ trunk/getPosPlot.php
@@ -65,7 +65,8 @@ for ($j = 0; $j < $count; $j++) {
 }
 
 $count_xy = 0;
-for ($l = 0; $l < $count; $l++) {
+//for ($l = 0; $l < $count; $l++) {
+for ($l = $count - 5; $l < $count; $l++) {
 	$date = $arrayX[$l]["ts"];
 	$data_cnt = count($keys);
 	
@@ -73,7 +74,7 @@ for ($l = 0; $l < $count; $l++) {
 		$arrayPlot[$count_xy]["date"] = $date;
 		//arrayPlot[$count_xy]["xval"] = $arrayX[$l]["$k"];
 		//$arrayPlot[$count_xy]["yval"] = $arrayY[$l]["$k"];
-		$arrayPlot[$count_xy]["yval"] = $k;
+		$arrayPlot[$count_xy]["yval"] = -$k;
 		//$arrayPlot[$count_xy]["yval"] = $arrayX[$l]["$k"];
 		$arrayPlot[$count_xy]["xval"] = $arrayY[$l]["$k"];
 		$count_xy++;
