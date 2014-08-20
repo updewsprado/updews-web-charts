26b918a331afdbc079c1f8acea3c7b59ee5647f5
updews.prado@gmail.com <updews.prado@gmail.com@e0b541cd-f2c1-ab39-7beb-506130f5bc63>
2014-08-20 10:33:32 +0000

    - "getSiteMaxNodes.php", PHP can now get an organized data which contains the site name and its max number of nodes

--- getSiteMaxNodes.php
+++ getSiteMaxNodes.php
@@ -1,32 +1,32 @@
 <?php
 require_once('connectDB.php'); 
 
-header('Content-type: application/json');
-
-if(isset($_GET['site'])) {
-	$site = $_GET['site'];
-}
-else {
-	$site = "blcb";
-} 
-
 // Create connection
-$con=mysqli_connect($host, $user, $pass, $db);
+$con=mysqli_connect($mysql_host, $mysql_user, $mysql_password, $mysql_database);
 
 // Check connection
 if (mysqli_connect_errno()) {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
-$sql_maxnode = "SELECT * FROM site_column_props WHERE s_id IN (SELECT s_id FROM site_column WHERE name = '" . $site . "') ;";
-$result = mysqli_query($con, $sql_maxnode);
-$maxnode;
+$sql_sites = "SELECT name FROM site_column;";
+$result = mysqli_query($con, $sql_sites);
+$sitesAll = [];
 
-$row = mysqli_fetch_array($result);
-$maxnode = $row['num_nodes'];
+$ctr_nodes = 0;
+while($row = mysqli_fetch_array($result)) {
+	$sitesAll[$ctr_nodes]['site'] = $row['name'];
+	
+	$sql_maxnode = "SELECT * FROM site_column_props WHERE s_id IN (SELECT s_id FROM site_column WHERE name = '" . $row['name'] . "') ;";
+	$resultNode = mysqli_query($con, $sql_maxnode);
+	$rowNode = mysqli_fetch_array($resultNode);
+	$sitesAll[$ctr_nodes]['nodes'] = $rowNode['num_nodes'];
+	
+	$ctr_nodes = $ctr_nodes + 1;
+}
  
 // Print it out as JSON
-echo json_encode($maxnode);
+echo json_encode($sitesAll);
 ?>
 
 
