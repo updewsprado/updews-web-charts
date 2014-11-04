65b2080c2b2f3d24693f7c5d071e30f985085282
updews.prado@gmail.com <updews.prado@gmail.com@e0b541cd-f2c1-ab39-7beb-506130f5bc63>
2014-11-04 05:14:46 +0000

    - Rewrote Data Presence Map Code + Added SVG Icons for future node status warnings

--- ver_2_gold/codeigniter/application/views/gold/monitoring.php
+++ ver_2_gold/codeigniter/application/views/gold/monitoring.php
@@ -67,7 +67,16 @@
                 <!-- /.row -->
 
                 <div class="row">
-                    
+                    <div class="col-lg-12">
+                    	<div class="panel panel-default">
+                            <div class="panel-heading">
+                                <h3 class="panel-title"><i class="fa fa-bar-chart-o fa-fw"></i> Data Presence Map</h3>
+                            </div>
+                            <div class="panel-body">
+                                <div id="presence-map-canvas"></div>
+                            </div>
+                        </div>
+                    </div>                    
                 </div>
                 <!-- /.row -->
 
