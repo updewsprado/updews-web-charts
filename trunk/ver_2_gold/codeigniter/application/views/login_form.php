8db0e221f9b3f252e1ea1df466fdeaf112a15ce2
updews.prado@gmail.com <updews.prado@gmail.com@e0b541cd-f2c1-ab39-7beb-506130f5bc63>
2014-11-19 07:59:21 +0000

    - Display First Name of User on Account Details - Made "login page" the default page

--- trunk/ver_2_gold/codeigniter/application/views/login_form.php
+++ trunk/ver_2_gold/codeigniter/application/views/login_form.php
@@ -5,6 +5,6 @@
 	echo form_input('username', 'Username');
 	echo form_password('password', 'Password');
 	echo form_submit('submit', 'Login');
-	echo anchor('lin/signup', 'Create Account');
+	//echo anchor('lin/signup', 'Create Account');
 	?>
 </div>
--- trunk/ver_2_gold/codeigniter/application/views/members_area.php
+++ trunk/ver_2_gold/codeigniter/application/views/members_area.php
@@ -1,2 +1,2 @@
 <h1>Welcome Back, <?php echo $first_name; ?>!</h1>
-<p>Click here to <a href="logout">logout</a></p>
+<p>Click here to <a href="/gold/logout">logout</a></p>
