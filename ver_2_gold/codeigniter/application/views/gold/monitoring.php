198156080a4da246131ba7f828c24b8b5cb99f44
updews.prado@gmail.com <updews.prado@gmail.com@e0b541cd-f2c1-ab39-7beb-506130f5bc63>
2015-01-28 03:27:22 +0000

    - Initial integration of node report status on Gold Version

--- ver_2_gold/codeigniter/application/views/gold/monitoring.php
+++ ver_2_gold/codeigniter/application/views/gold/monitoring.php
@@ -65,7 +65,7 @@
                                 <h3 class="panel-title"><i class="fa fa-bar-chart-o fa-fw"></i> Accelerometer Movement Alert Map 
                                 	<input type="button" id="alertLegend" onclick="alertLegends(this.form)" value="Show Legends" />
                                 	<button type="button" class="btn btn-sm btn-link"><a href="/ajax/csvmonitoring/lsb7days.csv">(Historical Data)</a></button>
-                                	<a href="#"><img class="imgreport" src="/<?php echo $imgfolder; ?>/report.svg" alt="" /></a>
+                                	<a href="<?php echo base_url() . $version; ?>/nodereport"><img class="imgreport" src="/<?php echo $imgfolder; ?>/report.svg" alt="" /></a>
                                 </h3>
 								<div id="alertcanvaslegend"  style="width:300px; height:100px; visibility:hidden; display:none;">
 									<svg width="290px" height="95px">
