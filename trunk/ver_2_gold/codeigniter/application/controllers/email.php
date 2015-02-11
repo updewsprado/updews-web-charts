42f8058d4636395f941d7fdcbdfb03a7a2704f3d
updews.prado@gmail.com <updews.prado@gmail.com@e0b541cd-f2c1-ab39-7beb-506130f5bc63>
2015-02-11 07:05:41 +0000

    + Email Controller for email auto sender

---
+++ trunk/ver_2_gold/codeigniter/application/controllers/email.php
@@ -0,0 +1,77 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Email extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function index()
+	{
+		$config = Array(
+			'protocol' => 'smtp',
+			'smtp_host' => 'ssl://smtp.googlemail.com',
+			'smtp_port' => 465,
+			'smtp_user' => 'updews.prado@gmail.com',
+			'smtp_pass' => 'september172013'
+		);
+		
+		$this->load->library('email', $config);
+		$this->email->set_newline("\r\n");
+		
+		$this->email->from('updews.prado@gmail.com', 'Prado Bognot');
+		$this->email->to('updews.prado@gmail.com');
+		$this->email->subject('This is an email test');
+		$this->email->message('It is working. Great!');
+		
+		$path = $this->config->item('server_root');
+		$file = $path . '/attachments/yourInfo.txt';
+		
+		$this->email->attach($file);
+		
+		if ($this->email->send()) {
+			echo "Your email was sent, handsome";
+		}
+		else {
+			show_error($this->email->print_debugger());
+		}
+	}
+}
+
+/* End of file email.php */
+/* Location: ./application/controllers/email.php */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
