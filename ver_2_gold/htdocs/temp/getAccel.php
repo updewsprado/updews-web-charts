76957733ea4ad39e361135000f08b01ba23f5eb6
updews.prado@gmail.com <updews.prado@gmail.com@e0b541cd-f2c1-ab39-7beb-506130f5bc63>
2015-03-18 05:53:18 +0000

    - Included the missing "node id" for the purged data pulling API

--- ver_2_gold/htdocs/temp/getAccel.php
+++ ver_2_gold/htdocs/temp/getAccel.php
@@ -28,6 +28,46 @@
 	   mysqli_close($con);
 	}
 	
+	function getAccelSite($q, $site, $host, $db, $user, $pass) {
+		// Create connection
+		$con=mysqli_connect($host, $user, $pass, $db);
+		
+		// Check connection
+		if (mysqli_connect_errno()) {
+		  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+		}
+
+		$sql = "SELECT * FROM $site WHERE timestamp > '".$q."' ORDER BY timestamp ASC";
+		$result = mysqli_query($con, $sql);
+
+		$dbreturn;
+		$ctr = 0;
+		
+		echo "timestamp,id,xvalue,yvalue,zvalue,mvalue" . "\n";
+		
+		while($row = mysqli_fetch_array($result)) {
+			$dbreturn[$ctr]['timestamp'] = $row['timestamp'];
+			$dbreturn[$ctr]['id'] = $row['id'];
+			$dbreturn[$ctr]['xvalue'] = $row['xvalue'];
+			$dbreturn[$ctr]['yvalue'] = $row['yvalue'];
+			$dbreturn[$ctr]['zvalue'] = $row['zvalue'];
+			$dbreturn[$ctr]['mvalue'] = $row['mvalue'];
+			
+			echo $dbreturn[$ctr]['timestamp'] . ",";
+			echo $dbreturn[$ctr]['id'] . ",";
+			echo $dbreturn[$ctr]['xvalue'] . ",";
+			echo $dbreturn[$ctr]['yvalue'] . ",";
+			echo $dbreturn[$ctr]['zvalue'] . ",";
+			echo $dbreturn[$ctr]['mvalue'] . "\n";
+
+			$ctr = $ctr + 1;
+		}
+
+		//echo json_encode( $dbreturn );
+
+		mysqli_close($con);
+	}
+	
 	function getAccel2($from, $to, $site, $nid, $host, $db, $user, $pass) {
 		// Create connection
 		$con=mysqli_connect($host, $user, $pass, $db);
