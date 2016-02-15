ef67891626b6dee7761625324170cb668c6acacd
updewsprado <updews.prado@gmail.com>
2016-02-15 17:11:53 +0800

    - readdata() now uses "Pubrelease_model"

--- ver_2_gold/codeigniter/application/controllers/pubrelease.php
+++ ver_2_gold/codeigniter/application/controllers/pubrelease.php
@@ -92,12 +92,6 @@ class Pubrelease extends CI_Controller {
 	// Read data from public alerts table
 	public function readdata()
 	{
-		// Database login information
-		$servername = "localhost";
-		$username = "updews";
-		$password = "october50sites";
-		$dbname = "senslopedb";
-
 		//get site data
 		if(isset($_GET['site'])) {
 		    $site = $_GET["site"];
@@ -107,42 +101,14 @@ class Pubrelease extends CI_Controller {
 		    return;
 		}
 
-		//echo "Received Data: $timestamp, $site, $alert, $timeRelease, $comments, $recipient, $acknowledged, $flagger";
+		$publicAlerts = $this->Pubrelease_model->getPublicAlerts($site);
 
-		// Create connection
-		$conn = mysqli_connect($servername, $username, $password, $dbname);
-
-		// Check connection
-		if (!$conn) {
-		    die("Connection failed: " . mysqli_connect_error());
-		}
-
-		$sql = "SELECT * FROM public_alert WHERE site = '$site' ORDER BY entry_timestamp DESC";
-		$result = mysqli_query($conn, $sql);
-
-		$ctr = 0;
-		$siteAlertPublic = [];
-		if (mysqli_num_rows($result) > 0) {
-		    // output data of each row
-		    while($row = mysqli_fetch_assoc($result)) {
-		        $siteAlertPublic[$ctr]["alert_id"] = $row["public_alert_id"];
-		        $siteAlertPublic[$ctr]["ts_data"] = $row["entry_timestamp"];
-		        $siteAlertPublic[$ctr]["name"] = $row["site"];
-		        $siteAlertPublic[$ctr]["internal_alert"] = $row["internal_alert_level"];
-		        $siteAlertPublic[$ctr]["ts_post_creation"] = $row["time_released"];
-		        $siteAlertPublic[$ctr]["recipient"] = $row["recipient"];
-		        $siteAlertPublic[$ctr]["acknowledged"] = $row["acknowledged"];
-		        $siteAlertPublic[$ctr]["flagger"] = $row["flagger"];
-		        $ctr++;
-		    }
-
-		    echo json_encode($siteAlertPublic); 
+		if ($publicAlerts == "[]") {
+			echo "Variable is empty<Br><Br>";
 		}
 		else {
-		    echo 0;
+			echo "$publicAlerts";
 		}
-
-		mysqli_close($conn);
 	}
 
 	// Update data in public alerts table
